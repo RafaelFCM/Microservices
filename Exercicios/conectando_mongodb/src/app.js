2c379a4531733b509da12c0e42b7c88425678122
RafaelFCM <01rafamiranda@gmail.com>
2024-10-10 17:10:34 -0300

    upload de varios arquivos

--- Exercicios/conectando_mongodb/src/app.js
+++ Exercicios/conectando_mongodb/src/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
-
 const app = express();
+const productRouter = require('./routes/product-route')
+
 
 app.use(express.json());
 //Converte o corpo da requisição para JSON, permitindo que o servidor entenda e manipule dados enviados em formato JSON.
 
 app.use(express.urlencoded({ extended: true }));
 //Permite que a API aceite requisições de formulários HTML.
+
+//Configurando a conexão
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/mydatabase');
+
 
 // Habilita o CORS (Cross-Origin Resource Sharing) que é uma política de segurança do navegador.
 app.use((req, res, next) => {
@@ -17,9 +23,13 @@ app.use((req, res, next) => {
 });
 
 //registar as models
+require('./models/product')
 
 //criar rotas
-const index = require('./routes/index')
-app.use('/', index)
+// const index = require('./routes/index')
+// app.use('/', index)
+
+
+app.use('/products', productRouter)
 
 module.exports = app;
